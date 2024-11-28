fa30825454f9625ebc37d2c15895430d871ba580
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2024-11-28 22:24:37 +0300

    Kurulum-2

--- cypress-proje/src/App.jsx
+++ cypress-proje/src/App.jsx
@@ -1,13 +1,19 @@
-import './App.css'
+import { BrowserRouter, Router, Route } from "react-router-dom";
+import Login from "../../components/Login.jsx";
+import Success from "../../components/Success.jsx";
+import ErrorPage from "../../components/ErrorPage.jsx";
+
 
 function App() {
-  
-
   return (
-    <>
-     
-    </>
-  )
+    <BrowserRouter>
+      <Router>
+        <Route path="/" element={<Login />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/error" element={<ErrorPage />} />
+      </Router>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
